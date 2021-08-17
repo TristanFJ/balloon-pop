e1745b95d230affcf4a5dc1ac9a7181863bcc982
TristanFJ <TristanJohns@protonmail.com>
2021-08-17 14:05:13 -0600

    balloon pop logic

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+let clickCount = 0
+let height = 120
+let width = 100
+let inflationRate = 20
+let maxSize = 150
+let popCount = 0
+
 function inflate (){
-    console.log("hello world")
+    clickCount++
+    let balloonElement = document.getElementById("balloon")
+    height += inflationRate
+    width += inflationRate
+    
+    if(height >= maxSize) {
+    console.log("pop")  
+    popCount++
+    height = 0
+    width = 0  
+    document.getElementById('popCount').innerText = popCount
+    }
+
+    balloonElement.style.height = height + "px"
+    balloonElement.style.width = width + "px"
+
+    let clickCountElem = document.getElementById("clickCount")
+    clickCountElem.innerText = clickCount.toString()
 }
